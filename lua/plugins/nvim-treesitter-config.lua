35d8adee582c7bc15318383912c1a4a9aa693694
kevinjteague <kevinjamesteague@gmail.com>
2026-06-12 02:57:43 -0400

    Fixed some formatting plugin issues and added rust debugging capabilities (WIP).

--- lua/plugins/nvim-treesitter-config.lua
+++ lua/plugins/nvim-treesitter-config.lua
@@ -1,19 +1,30 @@
 local M = {}
 
 local parsers = {
-    'java', 'c', 'lua', 'vim', 'vimdoc',
-    'html', 'yaml', 'rust', 'cpp', 'json',
-    'markdown', 'markdown_inline', 'csv',
+    "java",
+    "c",
+    "lua",
+    "vim",
+    "vimdoc",
+    "html",
+    "yaml",
+    "rust",
+    "cpp",
+    "json",
+    "markdown",
+    "markdown_inline",
+    "csv",
+    "slint",
 }
 
 function M.setup()
-    local treesitter = require('nvim-treesitter')
+    local treesitter = require("nvim-treesitter")
     treesitter.setup()
     -- Initial parser install. Subsequent installs on plugin update are
     -- handled by the PackChanged autocmd in pack-plugins.lua.
     treesitter.install(parsers)
 
-    vim.api.nvim_create_autocmd('FileType', {
+    vim.api.nvim_create_autocmd("FileType", {
         pattern = parsers,
         callback = function()
             vim.treesitter.start()
