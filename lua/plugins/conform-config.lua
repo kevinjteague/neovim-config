7511a1ac2ee8cc5ccb857fc01251a11e8c47923d
kevinjteague <kevinjamesteague@gmail.com>
2026-06-12 02:57:43 -0400

    Fixed some formatting plugin issues and added rust debugging capabilities (WIP).

--- lua/plugins/conform-config.lua
+++ lua/plugins/conform-config.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.setup()
-    local conform = require('conform')
+    local conform = require("conform")
 
     conform.setup({
         notify_on_error = false,
@@ -10,22 +10,22 @@ function M.setup()
             if disable_filetypes[vim.bo[bufnr].filetype] then
                 return nil
             else
-                return { timeout_ms = 500, lsp_format = 'fallback' }
+                return { timeout_ms = 500, lsp_format = "fallback" }
             end
         end,
         formatters_by_ft = {
-            lua = { 'stylua' },
+            lua = { "stylua" },
         },
     })
 
-    vim.keymap.set('', '<leader>f', function()
-        conform.format({ async = true, lsp_format = 'fallback' })
-    end, { desc = '[F]ormat buffer' })
+    vim.keymap.set("", "<leader>f", function()
+        conform.format({ async = true, lsp_format = "fallback" })
+    end, { desc = "[F]ormat buffer" })
 
     -- Also run format on BufWritePre (conform handles the async/sync split internally)
-    vim.api.nvim_create_autocmd('BufWritePre', {
+    vim.api.nvim_create_autocmd("BufWritePre", {
         callback = function(args)
-            conform.format({ bufnr = args.buf, lsp_format = 'fallback', timeout_ms = 500 })
+            conform.format({ bufnr = args.buf, lsp_format = "fallback", timeout_ms = 500 })
         end,
     })
 end
